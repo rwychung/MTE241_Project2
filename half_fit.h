5f6050f530da3419b1d1626ef3190da5aa6856af
Sam Wong <awesomesillyawesome@gmail.com>
2014-10-09 22:57:37 -0400

    HELLO WORLD
    added placeInBucket function
    changed buckets[] type to hold head and tail addresses

--- half_fit.h
+++ half_fit.h
@@ -5,6 +5,11 @@
 
 typedef uint32_t mem_block_t;
 
+typedef struct bucket_ptr{
+	int head;
+	int tail;
+} bucket_t;
+
 void  half_init( void );
 void *half_alloc( int );
 // or void *half_alloc( unsigned int );
